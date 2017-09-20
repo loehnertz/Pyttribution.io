082595a1163a4ca91aad2af5a5277cc950830696
loehnertz <jakob2102@gmail.com>
2017-09-20 16:17:08 +0200

    Added 'setup.py' as well as 'setup.cfg' for publishing the module on PyPi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyttributionio',
+    packages=['pyttributionio'],
+    version='0.1',
+    description='A Python wrapper around the Attribution.io API',
+    author='Jakob Löhnertz',
+    author_email='help@jakob.codes',
+    url='https://github.com/loehnertz/Pyttribution.io',
+    download_url='https://github.com/loehnertz/Pyttribution.io/archive/0.1.tar.gz',
+    keywords=['attribution.io', 'api', 'api-wrapper'],
+    classifiers=[],
+)
