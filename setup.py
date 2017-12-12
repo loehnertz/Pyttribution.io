0650c962a86934f13e33fd8ac0d7e093ecca1d0c
loehnertz <jakob2102@gmail.com>
2017-12-12 13:39:22 +0100

    Bumped the version number

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from distutils.core import setup
 setup(
     name='pyttributionio',
     packages=['pyttributionio'],
-    version='0.3.2',
+    version='0.4',
     description='A Python wrapper around the Attribution.io API',
     author='Jakob Löhnertz',
     author_email='help@jakob.codes',
     license='MIT',
     url='https://github.com/loehnertz/Pyttribution.io',
-    download_url='https://github.com/loehnertz/Pyttribution.io/archive/0.3.2.tar.gz',
+    download_url='https://github.com/loehnertz/Pyttribution.io/archive/0.4.tar.gz',
     keywords=['attribution.io', 'api', 'api-wrapper'],
     classifiers=[],
 )
